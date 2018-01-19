a562cdc04bac15fdd302fce89020ec83fa988899
pavelshermayster <sherm84@gmail.com>
2018-01-19 18:38:51 +0200

    button fix

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import React from 'react';
 const button = (props) => (
     <button
         disabled={props.disabled}
-        className={props.clases ? props.clases.join(' ') + 'Button' : 'Button'}
+        className={props.clases ? props.clases.join(' ') + ' Button' : 'Button'}
         onClick={props.clicked}>{props.children}</button>
 );
 
